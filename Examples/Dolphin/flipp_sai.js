4e3624116c324a553a4342eabe4ecc9ebf55bbf4
Andreas Plesch <andreasplesch@users.noreply.github.com>
2018-10-25 17:11:40 -0400

    Create flipp_sai.js

---
+++ Examples/Dolphin/flipp_sai.js
@@ -0,0 +1,9 @@
+var browser = X3D.getBrowser(X3D.createBrowser());
+var scene = browser.createScene();
+scene.setProfile = 'Immersive';
+//specification version only from xml attribute or vrml, is readOnly per spec.
+scene.setMetaData('title','flipp.x3d');
+//add others
+
+
+
